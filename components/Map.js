4742602ea11a1151b46538e87c0bf311a9e54c61
warren yun <xxwarrenhdxx@gmail.com>
2021-09-10 19:54:19 -0400

    Google map

--- components/Map.js
+++ components/Map.js
@@ -6,3 +6,21 @@ import {
     Marker
 } from 'react-google-maps';
 
+const Map = () => {
+    const RegularMap = withScriptjs(
+        withGoogleMap(props => (
+            <GoogleMap 
+                defaultZoom={15}
+            >
+            
+            </GoogleMap>
+        ))
+    );
+
+    return (
+        <RegularMap
+        />
+    );
+};
+
+export default Map;
