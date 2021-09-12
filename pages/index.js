16b00786f0fa1e5425ee7f3313810021ddb64e3a
warren yun <xxwarrenhdxx@gmail.com>
2021-09-12 09:45:33 -0400

    Implement data fetching for site

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,35 @@ import 'tailwindcss/tailwind.css';
 
 import axios from 'axios';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.data.data.user)
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-gray-800 font-bold text-6xl">hello</h1>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const getData = await axios ({
+    method: 'GET',
+    url: 'http://localhost:4000/graphql',
+    data: {
+      query: `
+        query {
+          user(userID: "1") {
+            id
+          }
+        }
+      `
+    }
+  });
+
+  const data = getData.data
+  
+  return {
+    props: {
+      data
+    }
+  }
 }
