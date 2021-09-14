efa4fc33c4d8a5018b50f6528583afc1ae59da46
warren yun <xxwarrenhdxx@gmail.com>
2021-09-13 21:06:09 -0400

    Basic sign in page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import 'tailwindcss/tailwind.css';
 
 export default function SignIn(props) {
-  
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-gray-800 font-bold text-6xl">Meetup Map</h1>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-gray-800 font-bold text-6xl my-20">Meetup Map</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+        Sign in
+      </button>
     </div>
   )
 }
